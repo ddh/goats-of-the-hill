f414a232170b206d7221505eaffd24a627c69613
Reid Thompson <rpt265@uw.edu>
2016-01-31 12:34:26 -0800

    added comments in code to clarify w/ Marriott.

--- js/background.js
+++ js/background.js
@@ -1,16 +1,18 @@
 function Background(game) {
-    Entity.call(this, game, 0, 400);
-    this.radius = 200;
+    Entity.call(this, game, 0, 0); // changed last param from 400
+    this.radius = 200; // TODO: double-check w/ Marriott that radius is used for drawing bounding circles
+    this.game = game;
+    this.ctx = game.ctx;
 }
 
 Background.prototype = new Entity();
 Background.prototype.constructor = Background;
 
-Background.prototype.update = function () { // TODO: fill in w/ own code???
+Background.prototype.update = function () {
 };
 
-Background.prototype.draw = function (ctx) {
-    ctx.fillStyle = "SaddleBrown";
-    ctx.fillRect(0,500,800,300);
-    Entity.prototype.draw.call(this); // TODO: what is this necessary for?
+Background.prototype.draw = function () { // deleted ctx parameter and put it as a field of the Background object
+    // TODO: draw background image given ctx
+
+    Entity.prototype.draw.call(this);
 };

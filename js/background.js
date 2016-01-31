b224b98f70b5c7402d8cb7dd4c08248634af3b44
Reid Thompson <rpt265@uw.edu>
2016-01-31 13:12:27 -0800

    background image will now be drawn. image itself is temporary, just a placeholder.

--- js/background.js
+++ js/background.js
@@ -1,18 +1,18 @@
-function Background(game) {
+function Background(game, image) {
     Entity.call(this, game, 0, 0); // changed last param from 400
-    this.radius = 200; // TODO: double-check w/ Marriott that radius is used for drawing bounding circles
+    //this.radius = 200; // TODO: double-check w/ Marriott that radius is used for drawing bounding circles
     this.game = game;
     this.ctx = game.ctx;
+    this.image = image;
 }
 
 Background.prototype = new Entity();
 Background.prototype.constructor = Background;
 
-Background.prototype.update = function () {
-};
+// currently, background image isn't being updated - static image
+Background.prototype.update = function () {};
 
 Background.prototype.draw = function () { // deleted ctx parameter and put it as a field of the Background object
-    // TODO: draw background image given ctx
-
+    this.ctx.drawImage(this.image, 0, 0);
     Entity.prototype.draw.call(this);
 };

46dfdb6d41ab0a821e3c79fa69d4fd16c45f57a9
Reid Thompson <rpt265@uw.edu>
2016-01-30 16:47:53 -0800

    added comments for things to be added based on code deleted from Marriott.

--- js/main.js
+++ js/main.js
@@ -2,24 +2,17 @@
 
 // asset manager is now constructed after class is defined (bottom of assetmanager.js)
 
-ASSET_MANAGER.queueDownload("./img/960px-Blank_Go_board.png");
-ASSET_MANAGER.queueDownload("./img/black.png");
-ASSET_MANAGER.queueDownload("./img/white.png");
+// TODO: queue downloads
 
 ASSET_MANAGER.downloadAll(function () {
-    console.log("starting up da sheild");
     var canvas = document.getElementById('gameWorld');
     var ctx = canvas.getContext('2d');
 
-
     var gameEngine = new GameEngine();
-    var circle = new Circle(gameEngine);
-    circle.setIt();
-    gameEngine.addEntity(circle);
-    for (var i = 0; i < 12; i++) {
-        circle = new Circle(gameEngine);
-        gameEngine.addEntity(circle);
-    }
+
+    // TODO: initialize entities
+    // TODO: add entities to game engine
+
     gameEngine.init(ctx);
     gameEngine.start();
 });

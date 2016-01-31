4b2b4a7558bbc6bb380376335842b3120eaf4098
Reid Thompson <rpt265@uw.edu>
2016-01-31 13:10:05 -0800

    added background entity to game engine, to be tested later.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 // asset manager is now constructed after class is defined (bottom of assetmanager.js)
 
 // TODO: queue downloads
+ASSET_MANAGER.queueDownload("../img/canvas-meadow.png"); // temporary background image for testing
 
 ASSET_MANAGER.downloadAll(function () {
     var canvas = document.getElementById('gameWorld');
@@ -11,7 +12,9 @@ ASSET_MANAGER.downloadAll(function () {
     var gameEngine = new GameEngine();
 
     // TODO: initialize entities
+    var bg = new Background(game, ASSET_MANAGER.getAsset("../img/canvas-meadow.png"));
     // TODO: add entities to game engine
+    gameEngine.addEntity(bg);
 
     gameEngine.init(ctx);
     gameEngine.start();

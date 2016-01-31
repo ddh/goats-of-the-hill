465b9f638f21c7dd7373d03a45ca8e1c17337b81
Reid Thompson <rpt265@uw.edu>
2016-01-30 16:47:08 -0800

    took out marriott's calculations of current frame's (x,y) coordinates.

--- js/animation.js
+++ js/animation.js
@@ -7,23 +7,19 @@ function Animation(spriteSheet, frameWidth, frameHeight, frameDuration, frames, 
     this.totalTime = frameDuration * frames;
     this.elapsedTime = 0;
     this.loop = loop;
-    this.reverse = reverse; // Marriott's unicorn animation: his spritesheet's frames were in reserve order
+    this.reverse = reverse; // might as well keep this in here, just in case it's needed
 }
 
 Animation.prototype.drawFrame = function (tick, ctx, x, y) {
     this.elapsedTime += tick;
     if (this.isDone()) {
-        if (this.loop) this.elapsedTime = 0; // if this is a looping animation, reset timer for next time anim happens
+        if (this.loop) this.elapsedTime = 0;
     }
     var frame = this.currentFrame();
     var xindex = 0;
     var yindex = 0;
-    if (frame > 13) {
-        frame = 26 - frame; // TODO: there were 26 frames in his spritesheet?
-    }
-    // calculating current frame's x/y coords
-    xindex = frame % 5;
-    yindex = Math.floor(frame / 5);
+
+    // TODO: calculate current frame's x/y coords for drawing
 
     console.log(frame + " " + xindex + " " + yindex);
 

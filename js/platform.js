92feeca00430a8a15a58cdad5982dcf9f1305efa
Reid Thompson <rpt265@uw.edu>
2016-01-31 17:10:27 -0800

    other changes to platform code.

--- js/platform.js
+++ js/platform.js
@@ -1,3 +1,37 @@
-/**
- * Created by reidobandedo on 1/31/16.
- */
+// This code is based on Chris Marriott's Unicorn game found here:
+// https://github.com/algorithm0r/GamesProject/blob/Unicorn/game.js
+
+function Platform(game, x, y, width, height) {
+    this.width = width;
+    this.height = height;
+    this.startX = x;
+    this.startY = y;
+    this.boundingbox = new BoundingBox(x, y, width, height);
+    Entity.call(this, game, x, y);
+}
+
+Platform.prototype = new Entity();
+Platform.prototype.constructor = Platform;
+
+Platform.prototype.reset = function () {};
+
+// don't need to update our platforms as their state won't change after the game is initialized
+Platform.prototype.update = function () {};
+
+Platform.prototype.draw = function (ctx) {
+    // TODO: would be cool to draw platforms with gradients like in code below
+
+    /*
+    var grad;
+    grad = ctx.createLinearGradient(0, this.y, 0, this.y + this.height);
+    grad.addColorStop(0, 'red');
+    grad.addColorStop(1 / 6, 'orange');
+    grad.addColorStop(2 / 6, 'yellow');
+    grad.addColorStop(3 / 6, 'green')
+    grad.addColorStop(4 / 6, 'aqua');
+    grad.addColorStop(5 / 6, 'blue');
+    grad.addColorStop(1, 'purple');
+    ctx.fillStyle = grad;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+    */
+};

7101aa4bd3b83b0f6b25ef5d3894d258903ac85a
Reid Thompson <rpt265@uw.edu>
2016-01-31 17:29:37 -0800

    updated some layout stuff on the game page so we can keep track of rounds played.

--- js/main.js
+++ js/main.js
@@ -7,29 +7,19 @@ ASSET_MANAGER.queueDownload("./img/canvas-meadow.png"); // temporary background 
 
 ASSET_MANAGER.downloadAll(function () {
     var canvas = document.getElementById('gameWorld');
+    var roundNumber = document.getElementById('roundNumber');
     var ctx = canvas.getContext('2d');
 
     var gameEngine = new GameEngine();
+    gameEngine.roundNumber = roundNumber;
 
-    // initializes Goat and Circle objects properly, binds Circle to Goat, and adds both to GameEngine
-    var initEngineWithGoatCirclePair = function () {
-        var goat = new Goat(gameEngine);
-        var circ = new Circle(gameEngine);
-        circ.setX(goat.width);
-        circ.setY(goat.height);
-        circ.makeCircleBeEntity();
-        circ.setRadius({x: goat.width, y: goat.height});
-        goat.setBoundingCircle(circ);
-        gameEngine.addEntity(goat);
-        gameEngine.addEntity(circ);
-    };
-
-    // TODO: here, initialize entities & add entities to game engine
+    // TODO: initialize entities
     var bg = new Background(gameEngine, ASSET_MANAGER.getAsset("./img/canvas-meadow.png"));
-    gameEngine.addEntity(bg);
+    var pg = new PlayGame(gameEngine, 320, 350);
 
-    // TODO: in loop for all goats to be created in game, call initEngineWithGoatCirclePair
-    //initEngineWithGoatCirclePair(); // just creating one goat and bounding circle for now
+    // TODO: add entities to game engine
+    gameEngine.addEntity(bg);
+    gameEngine.addEntity(pg);
 
     gameEngine.init(ctx);
     gameEngine.start();

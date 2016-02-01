42a662bc92ef5a57acb7ab7bfbd10343697eb978
Reid Thompson <rpt265@uw.edu>
2016-01-31 18:31:06 -0800

    merge conflicts fixed.

--- js/playgame.js
+++ js/playgame.js
@@ -2,12 +2,13 @@
 // https://github.com/algorithm0r/GamesProject/blob/Unicorn/game.js
 
 var ROUND_TIME_LIMIT = 300; // 5 minutes
-var TIMES_PLAYED = 0;
+var ROUNDS_PLAYED = 0;
 
 function PlayGame(game, x, y) {
     this.game = game;
     this.x = x;
     this.y = y;
+    this.game.roundNumber.innerHTML = "Round #" + (ROUNDS_PLAYED + 1);
     Entity.call(this, game, x, y);
 }
 
@@ -16,18 +17,19 @@ PlayGame.prototype.constructor = PlayGame;
 
 PlayGame.prototype.reset = function () {
     this.game.running = false;
-    TIMES_PLAYED++; // a game has been played previously because the game is getting reset
+    ROUNDS_PLAYED++; // a game has been played previously because the game is getting reset
+    this.game.roundNumber.innerHTML = "Round #" + (ROUNDS_PLAYED + 1);
 };
 
 PlayGame.prototype.update = function () {
-    if (this.game.click && this.game.timer.gameTime > ROUND_TIME_LIMIT) this.game.running = true;
+    if (this.game.click && this.game.timer.gameTime < ROUND_TIME_LIMIT) this.game.running = true;
 };
 
 PlayGame.prototype.draw = function (ctx) {
     if (!this.game.running) {
         ctx.font = "24pt Arial";
         if (this.game.mouse) { ctx.fillStyle = "purple"; }
-        if (TIMES_PLAYED === 0) {
+        if (ROUNDS_PLAYED === 0) {
             ctx.fillText("Play OMG!?!", this.x, this.y);
         } else {
             ctx.fillText("Play OMG Again?!?", this.x - 30, this.y);

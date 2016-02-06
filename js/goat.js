d624385c5776d1c4f5a12aed0a3f8d7b7b62cea2
Duy Huynh <dhuynh88@uw.edu>
2016-02-05 22:59:15 -0800

    goat almost moves with platforms

--- js/goat.js
+++ js/goat.js
@@ -18,6 +18,7 @@ function Goat(game) {
     this.velocity = {x: 0, y: 0};
     this.speed = 5;
     this.jumpHeight = 200;
+    this.platform = null;
 
     // Platforms
     this.platform = game.platforms[0]; // Ground platform to start
@@ -85,6 +86,14 @@ Goat.prototype.reset = function () {
 
 // Based off of Chris Marriott's Unicorn's update method: https://github.com/algorithm0r/GamesProject/blob/Unicorn/game.js
 Goat.prototype.update = function () {
+
+
+    // Update goat's velocities if it's on a platform
+    if(this.platform) {
+        this.x += this.platform.velocity.x;
+        this.y += this.platform.velocity.y;
+    }
+
     // Update goat's facing direction (LEFT or RIGHT)
     if (this.game.right) {
         this.right = true;

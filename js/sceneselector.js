e95dbe8a6634b5cc12efeec120566407d8ad9fad
Reid Thompson <rpt265@uw.edu>
2016-02-09 17:31:11 -0800

    adding scene and scene selector code, early version.

---
+++ js/sceneselector.js
@@ -0,0 +1,35 @@
+function Scene(platforms, background) {
+    this.platforms = platforms;
+    this.background = background;
+}
+
+Scene.prototype = new Entity();
+Scene.prototype.constructor = Scene;
+
+Scene.prototype.draw = function() {};
+
+Scene.prototype.update = function() {};
+
+function SceneSelector() {
+    this.scenes = [];
+    this.currentScene = 0;
+}
+
+SceneSelector.prototype = new Entity();
+SceneSelector.prototype.constructor = SceneSelector;
+
+SceneSelector.prototype.addScene = function(scene) {
+    this.scenes.push(scene);
+};
+
+SceneSelector.prototype.getFirstScene = function() {
+    return this.scenes[0];
+};
+
+SceneSelector.prototype.getNextScene = function() {
+    return this.scenes[++this.currentScene];
+};
+
+SceneSelector.prototype.draw = function() {};
+
+SceneSelector.prototype.update = function() {};

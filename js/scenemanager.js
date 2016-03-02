1306cbda86358429637918a4ecaa757743916cf8
Reid Thompson <rpt265@uw.edu>
2016-03-01 22:43:29 -0800

    initial pass thru of scene selector and scene superclass.

--- js/scenemanager.js
+++ js/scenemanager.js
@@ -6,48 +6,49 @@
  * Round: Background, Platforms, Goats, (& text/images drawn on Canvas)
  * Scoreboard: Background, (& text/images drawn on Canvas)
  * EndGame: Background, (& text/images drawn on Canvas)
- * TODO: add in Esc/Pause screen (has Objective, Controls, Description of powerups) ???
+ * TODO: add in Esc/Pause screen (has Objective, Controls, Description of powerups) ??? <-- let's do this later...
  */
 
-function Scene(game, background, nextScene) {
-    //this.platforms = platforms;
+function Scene(game, background, type) {
     this.game = game;
     this.background = background;
-    this.next = nextScene;
+    this.next = null; // field to be set in main ("on the fly")
+    this.type = type;
 }
 
 Scene.prototype = new Entity();
 Scene.prototype.constructor = Scene;
 
-// the Scene superclass is only responsible for drawing its Background (NEEDS TO BE AT TOP OF EVERY SUB-CLASSES' DRAW METHOD)
-Scene.prototype.draw = function (ctx) {
-    this.background.draw(ctx);
-};
-
 Scene.prototype.toString = function () {
     return "Scene";
 };
 
-Scene.prototype.nextScene = function () {
-    return this.next;
+var drawTextWithOutline = function (ctx, font, text, x, y, fillColor, outlineColor) {
+    ctx.font = font;
+    ctx.strokeStyle = outlineColor;
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'miter';
+    ctx.miterLimit = 5;
+    ctx.strokeText(text, x, y);
+    ctx.fillStyle = fillColor;
+    ctx.fillText(text, x, y);
 };
 
 /***********************************************
  *          START OF SCENE 'INTERFACE'         *
  ***********************************************/
 
-// performs variable initialization
 Scene.prototype.startScene = function () {};
 
-// performs cleanup performs
 Scene.prototype.endScene = function () {};
 
-// for Round, checks if timer is completed
-// for all others, checks if click has been detected
 Scene.prototype.isSceneDone = function () {};
 
-// this alteration of state logic happens in subclass
 Scene.prototype.update = function () {};
+
+Scene.prototype.draw = function (ctx) {};
+
+Scene.prototype.reset = function () {};
 
 /***********************************************
  *           END OF SCENE 'INTERFACE'          *
@@ -58,35 +59,49 @@ Scene.prototype.update = function () {};
  */
 
 // Class constants:
-var POWERUP_INTERVAL = 10;  // Every x sec a powerup spawns
 var ROUNDS_PLAYED = 0;
-var COLLECTIBLES = ['speedUp', 'doubleJump', 'highJump', 'maxCharge', 'attackUp', 'invincibility'];
-//var COLLECTIBLES = ['invincibility']; //TODO: Using this as a means to test a powerup individually. Just comment out the above.
 
-function SceneManager(scenes) {
-    this.scenes = scenes;
-    this.currentScene = null;
+function SceneManager(currentScene) {
+    this.currentScene = currentScene; // TODO: all Scenes for game stored in linkedlist attached to this head pointer
     this.goatScores = { // serves as temp storage for goat scores between rounds (data passed from scene to scene)
-        1: [],          // player 1
-        2: [],          // player 2
-        3: [],          // player 3
-        4: []           // player 4
+        0: [],          // player 1
+        1: [],          // player 2
+        2: [],          // player 3
+        3: []           // player 4
     };
-    this.currentScene = null;
 }
 
 SceneManager.prototype = new Entity();
 SceneManager.prototype.constructor = SceneManager;
 
-SceneManager.prototype.getNextScene = function() {
-    return this.currentScene.next;
-};
-
 SceneManager.prototype.update = function() {
-    // TODO: check if Scene is done, then start transition to next Scene
+    // check if Scene is done, then start transition to next Scene
+    if (this.currentScene.isDone()) {
+        if (this.currentScene.type === "Round") {
+            this.storeScoresFromCurrentRound();
+            ROUNDS_PLAYED++;
+        }
+        this.currentScene.endScene();
+        this.currentScene = this.currentScene.next;
+        this.currentScene.startScene();
+    } else { // else, if not done, continue updating current Scene
+        this.currentScene.update();
+    }
 };
 
-SceneManager.prototype.draw = function() {};
+SceneManager.prototype.storeScoresFromCurrentRound = function () {
+    for (var i = 0, len = this.currentScene.goats.length; i < len; i++) {
+        this.goatScores[i].push(this.currentScene.goats[i].score);
+    }
+};
+
+SceneManager.prototype.draw = function() {
+    this.currentScene.draw();
+};
+
+SceneManager.prototype.reset = function () {
+    this.currentScene.reset();
+};
 
 SceneManager.prototype.toString = function() {
     return "SceneManager";

fb867e93a0877a4f33690ad2a3c5fb4f53d3262d
such <suchk@live.com>
2016-03-02 18:09:46 -0800

    Added title screen. Changed drawRoundedRect to receive fillColor and outlineColor (updated all references that I found). Added a little bit of logic to scenemanager to call the first scene's startScene (needed for the title scene). Currently the tutorial button routes back to the title screen. This will need to change when we have a tutorial scene.

--- js/main.js
+++ js/main.js
@@ -108,7 +108,8 @@ var makeSceneManager = function (gameEngine) {
 
     // 2. Link up all Scenes in correct sequence before returning SceneManager with a reference to the title Scene
     // ---
-    titleScene.next = r1;   // TODO: link will change once Tutorial Scene added
+    titleScene.roundScene = r1;   // TODO: link will change once Tutorial Scene added
+    titleScene.tutorialScene = titleScene; // COMPLETE WHEN TUTORIAL SCENE IS DONE    
     r1.next = sb1;
     sb1.next = r2;
     r2.next = sb2;
